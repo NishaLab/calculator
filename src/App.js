19701ae2a771e3d64018a2920e60dbb469fcae7b
Nguyễn Đức Hưng <hungnguyenw420@gmail.com>
2020-05-28 13:35:32 +0700

    rough sketch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Button from "./components/Button";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="calc-wrapper">
+          <div className="row">
+            <Button>7</Button>
+            <Button>8</Button>
+            <Button>9</Button>
+            <Button>*</Button>
+          </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <div className="row">
+            <Button>4</Button>
+            <Button>5</Button>
+            <Button>6</Button>
+            <Button>-</Button>
+          </div>
+
+          <div className="row">
+            <Button>1</Button>
+            <Button>2</Button>
+            <Button>3</Button>
+            <Button>+</Button>
+          </div>
+
+          <div className="row">
+            <Button>.</Button>
+            <Button>0</Button>
+            <Button>/</Button>
+            <Button>=</Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import "./Button.css";
+
+class Button extends Component {
+  render() {
+    return <div className="button">{this.props.children}</div>;
+  }
+}
+export default Button;
